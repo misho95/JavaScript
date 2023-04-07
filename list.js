3ebeeec06733da097b70724d4c579184fa96be15
Misho <chapidze.misho95@gmail.com>
2023-04-07 11:47:28 +0400

    ss

--- list.js
+++ list.js
@@ -8,36 +8,73 @@ const selectLists = document.querySelector(`.lists`);
 formBtn.addEventListener(`click`, (event) => {
     event.preventDefault();
     const value = formInput.value;
-    const mapList = `
-    <div class="grid-left">${value}</div>
-    <div class="grid-right">
-        <button class="editbtn">Edit</button>
-        <button class="downbtn">Down</button>
-        <button class="upbtn">Up</button>
-    </div>`;
+        if(value !== ``){
+    const divGridLeft = document.createElement(`div`);
+    divGridLeft.className = `grid-left`;
+    divGridLeft.innerHTML = value;
+
+    const divGridRight = document.createElement(`div`);
+    divGridRight.className = `grid-right`;
+    const btnEdit = document.createElement(`button`);
+    btnEdit.className = `editbtn`;
+    btnEdit.innerHTML = `Edit`;
+    btnEdit.addEventListener(`click`, () => {
+        edit(btnEdit);
+    });
+    divGridRight.append(btnEdit);
+    const btnDown = document.createElement(`button`);
+    btnDown.className = `downbtn`;
+    btnDown.innerHTML = `Down`;
+    btnDown.addEventListener(`click`, () => {
+        moveDwn(btnDown);
+    })
+    divGridRight.append(btnDown);
+    const btnUp = document.createElement(`button`);
+    btnUp.className = `upbtn`;
+    btnUp.innerHTML = `Up`;
+    btnUp.addEventListener(`click`, () => {
+        moveu(btnUp);
+    })
+    divGridRight.append(btnUp);
 
     const div = document.createElement(`div`);
     div.className = `list`;
-    div.innerHTML = mapList;
+   div.appendChild(divGridLeft);
+   div.appendChild(divGridRight);
+    div.firstElementChild.addEventListener(`click`, ()=>{ 
+        select(div.firstElementChild);
+    })
     selectLists.appendChild(div);
     formInput.value = ``;
+    } else {
+        alert(`Input Is Empty`);
+    }
 });
 
 
 //selecting Lists
 
+function select(event){
+
+    const parent = event.parentNode;
+    const checkForClass = parent.classList.contains(`active-list`);
+
+    if(checkForClass === false){
+    parent.classList.add(`active-list`);
+    } else if(checkForClass === true){
+        parent.classList.remove(`active-list`);
+    }
+
+}
+
+
 const slList = document.querySelectorAll(`.list .grid-left`);
 
 slList.forEach((list) => {
     list.addEventListener(`click`, () => {
-        const parent = list.parentNode;
-        const checkForClass = parent.classList.contains(`active-list`);
 
-        if(checkForClass === false){
-        parent.classList.add(`active-list`);
-        } else if(checkForClass === true){
-            parent.classList.remove(`active-list`);
-        }
+        select(list);
+
     })
 });
 
@@ -58,41 +95,60 @@ removeBtn.addEventListener(`click`, (event) => {
 
 //Move Lists
 
-    //Move Up
+    //Move Down
+
+    function moveDwn(btn){
+        let parent = btn.parentNode;
+        parent = parent.parentNode;
+        const nextSibling = parent.nextElementSibling;
+        if(nextSibling !== null){
+        nextSibling.after(parent);
+        }
+    }
 
     const moveDown = document.querySelectorAll(`.downbtn`);
 
     moveDown.forEach((btn) => {
         btn.addEventListener(`click`, (event) => {
             event.preventDefault();
-            let parent = btn.parentNode;
-            parent = parent.parentNode;
-            const nextSibling = parent.nextElementSibling;
-            if(nextSibling !== null){
-            nextSibling.after(parent);
-            }
+            moveDwn(btn);
         })
     });
 
     
     //Move Up
 
+    function moveu(btn){
+        let parent = btn.parentNode;
+        parent = parent.parentNode;
+        const prevSibling = parent.previousElementSibling;
+        if(prevSibling){
+            prevSibling.before(parent);
+        }
+    }
+
     const moveUp = document.querySelectorAll(`.upbtn`);
 
     moveUp.forEach((btn) => {
         btn.addEventListener(`click`, (event) => {
             event.preventDefault();
-            let parent = btn.parentNode;
-            parent = parent.parentNode;
-            const prevSibling = parent.previousElementSibling;
-            if(prevSibling){
-                prevSibling.before(parent);
-            }
+            moveu(btn);
         })
     });
 
 
     //edit
+
+    function edit(btn){
+        let parent = btn.parentNode;
+        parent = parent.parentNode;
+        const child = parent.firstElementChild;
+        const value = child.textContent;
+        const newValue = prompt(`Edit List,`, value);
+        if(newValue !== ``){
+            child.textContent = newValue;
+        }
+    }
 
     
     const editBtn = document.querySelectorAll(`.editbtn`);
@@ -100,13 +156,6 @@ removeBtn.addEventListener(`click`, (event) => {
     editBtn.forEach((btn) => {
         btn.addEventListener(`click`, (event) => {
             event.preventDefault();
-            let parent = btn.parentNode;
-            parent = parent.parentNode;
-            const child = parent.firstElementChild;
-            const value = child.textContent;
-            const newValue = prompt(`Edit List,`, value);
-            if(newValue !== ``){
-                child.textContent = newValue;
-            }
+           edit(btn);
         })
     })
